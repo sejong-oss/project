f1df610ea6c8248e7cf22e3e5dbd6895caaa591d
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2026-05-10 16:39:56 +0900

    feat: TailwindCSS v4, Storybook 설정 추가

---
+++ frontend/.storybook/main.js
@@ -0,0 +1,7 @@
+export default {
+    stories: ["../src/stories/**/*.stories.@(js|jsx)"],
+    framework: {
+        name: "@storybook/react-vite",
+        options: {},
+    },
+};
---
+++ frontend/.storybook/preview.js
@@ -0,0 +1,17 @@
+import "../src/assets/index.css";
+
+export const parameters = {
+    options: {
+        storySort: {
+            order: ["Design System", ["Overview", "*"]],
+        },
+    },
+    backgrounds: {
+        default: "white",
+        values: [
+            { name: "white", value: "#ffffff" },
+            { name: "gray-50", value: "#fafaf9" },
+            { name: "gray-100", value: "#f4f2ee" },
+        ],
+    },
+};
